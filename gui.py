93aeb2e4451fc6405945e6ca0c2a163ef1ce89ce
KerorinNF <kitakitsune.gingitsune.8220a@gmail.com>
2022-08-24 22:22:08 +0900

    multi

--- gui.py
+++ gui.py
@@ -2,6 +2,10 @@ import dearpygui.dearpygui as dpg
 import os
 import configparser
 import jfif_to_jpg
+import extensions
+
+
+EXT_LIST = extensions.exts
 
 
 # 初期化
@@ -17,27 +21,31 @@ def converter(sender, app_data, user_data):
     path_name = dpg.get_value('path_name')
     ext_from = dpg.get_value('ext_from_name')
     ext_to = dpg.get_value('ext_to_name')
-    # パスが入力されてない場合
-    if path_name == '':
-        dpg.set_value(user_data, '!Please enter the path.!')
-    # アクセス可能パスじゃない場合
-    elif not base_path in path_name:
-        dpg.set_value(user_data, '!>>Cannot access this directory. try other directory path.!')
-    # ディレクトリが存在しない場合
-    elif not os.path.exists(path_name):
-        dpg.set_value(user_data, '!>>Image file path is different. try again.!')
     # 拡張子が間違っている場合
-    elif ext_from or ext_to:
-        dpg.set_value(user_data, '!File extention is different.!')
+    if not ext in ext_from or not ext in ext_to:
+        dpg.set_value(user_data, '!Extention is different.!')
+        flag = False
     else:
-        try:
-            success = jfif_to_jpg.convert(path_name, ext_from , ext_to)
-            if not success:
-                dpg.set_value(user_data, f'>>There are no {ext_from} image files.')
-            else:
-                dpg.set_value(user_data, '>>WELL DONE')
-        except:
-            dpg.set_value(user_data, '!Something is wrong. Cannot convert!')
+        flag = True
+    if flag == True:
+        # パスが入力されてない場合
+        if path_name == '':
+            dpg.set_value(user_data, '!Please enter the path.!')
+        # アクセス可能パスじゃない場合
+        elif not base_path in path_name:
+            dpg.set_value(user_data, '!>>Cannot access this directory. try other directory path.!')
+        # ディレクトリが存在しない場合
+        elif not os.path.exists(path_name):
+            dpg.set_value(user_data, '!>>Image file path is different. try again.!') 
+        else:
+            try:
+                success = jfif_to_jpg.convert(path_name, ext_from , ext_to)
+                if not success:
+                    dpg.set_value(user_data, f'>>There are no {ext_from} image files.')
+                else:
+                    dpg.set_value(user_data, '>>WELL DONE')
+            except:
+                dpg.set_value(user_data, '!Something is wrong. Cannot convert!')
 
 
 # メインウィンドウ設定
@@ -50,7 +58,7 @@ with dpg.window(label='main_window', tag='main_window'):
     dpg.add_input_text(label='Ext_from', tag='ext_from_name', width=100)
     # ext_to
     dpg.add_text(">>Input to-ext")
-    dpg.add_input_text(label='Ext_to', tag='ext_to_text', width=100)
+    dpg.add_input_text(label='Ext_to', tag='ext_to_name', width=100)
     dpg.add_button(label='Convert' ,callback=converter, user_data='result_text')
     dpg.add_separator()
     dpg.add_text(tag='result_text')
